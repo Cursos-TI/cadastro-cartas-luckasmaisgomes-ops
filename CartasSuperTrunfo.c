113e2ddc0555f5976e402751b3df2be9a1483320
L.u.c-Projects <luckasmaisgomes@gmail.com>
2025-10-21 17:02:53 +0000

    SuperTrundoMestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,6 +23,8 @@ int main() {
     float PIBperCapita1;
     float mediaD1;
     float mediaP1;
+    float SuperPoder1;
+
 
 // Área para definição das variáveis para armazenar as propriedades das cidades 2        
     
@@ -38,6 +40,8 @@ int main() {
     float PIBperCapita2;
     float mediaD2;
     float mediaP2;
+    float SuperPoder2;
+    float resultadoA, resultadoB;
 
 // Área para entrada de dados cidade 1
         printf("Digite o nome do estado n°1: \n");
@@ -64,7 +68,8 @@ int main() {
     mediaD1 = (float)(população1 / Area1) /2;
     mediaP1 = (float)(PIB1 / população1) /2;
 
-              
+    SuperPoder1 = (float)(população1 + Area1 + PIB1 + PIBperCapita1 + Densidade1 / PontosTuristicos1);
+    
 // Área para entrada de dados cidade 2
         printf("Digite o nome do estado n°2: \n");
         scanf("%s", &estado2);
@@ -90,25 +95,40 @@ int main() {
     mediaD2 = (float)(população2 / Area2) /2;
     mediaP2 = (float)(PIB2 / população2) /2;
 
+    SuperPoder2 = (float)(população2 + Area2 + PIB2 + PIBperCapita2 + Densidade2 / PontosTuristicos2);
+
 // Área para exibição dos dados da cidade 1
         printf("Estado 1: %s - Cidade: %s - Código: %s \n", estado1, cidade1, código1);
-        printf("Populção: %.3f \n", população1);
-        printf("Área: %.3f km² \n", Area1);
+        printf("Populção: %.3f - Área: %.3f \n", população1, Area1);
         printf("PIB: %.f bilhões de reais \n", PIB1);
         printf("Pontos Turisticos: %d \n", PontosTuristicos1);
         printf("Densidade Populacional: %.3f hab/km² \n", mediaD1);
         printf("PIB per capita: %.3f reais \n", mediaP1);
+        printf("Super Poder total é: %.3f \n", SuperPoder1);
+        
+        printf("\n");
 
 // Área para exibição dos dados da cidade 2
         printf("Estado 2: %s - Cidade: %s - Código: %s \n", estado2, cidade2, código2);
-        printf("Populção: %.3f \n", população2);
-        printf("Área: %.3f km² \n", Area2);
+        printf("Populção: %.3f - Área: %.3f \n", população2, Area2);
         printf("PIB: %.f bilhões de reais \n", PIB2);
         printf("Pontos Turisticos: %d \n", PontosTuristicos2);
         printf("Densidade Populacional: %.3f hab/km² \n", mediaD2);
         printf("PIB per capita: %.3f reais \n", mediaP2);
+        printf("Super Poder total é: %.3f \n", SuperPoder2);
 
+        printf("\n");
 
-    return 0;
+// Soma de Super poder!
+
+        resultadoA = 1; // se sim
+        resultadoB = 0; // se não
+
+        printf("A carta 1 vale: %.3f e a carta 2 vale: %.3f\n", SuperPoder1 , SuperPoder2);
+
+        printf("A carta mais forte é a 1: %d \n",SuperPoder1 >= SuperPoder2 );
+        printf("A carta mais forte é a 2: %d \n",SuperPoder1 <= SuperPoder2);
+          
+        return 0;
     
 } 
